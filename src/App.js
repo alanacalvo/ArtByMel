f9df18ba22c8a0c419a533e32450a834316750aa
aniecres <111096788+aniecres@users.noreply.github.com>
2023-03-17 13:11:42 -0400

    update hero sec code

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
        
-        <HeroSection></HeroSection>
+        <HeroSection/>
     </div>
   );
 }
--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import './HeroSection.scss';
+import Style from './HeroSection.scss';
 
 
 function HeroSection() {
+  
+
   return (
-  <>  
+  <> 
     <div className='hero-container'> 
+    {/* <img src="src/Components/HeroSection/abm-hero.png" alt="Paris" /> */}
+    <button className="book">Book With Melvin</button>   
      <div className='headline'>
      <h1>Melvin Lozada</h1>
      <div className='subtitle'>Get flawless Looks for any Occasion</div>
