55bc66421800477d3dd40f212489b6c6b7a55361
aniecres <111096788+aniecres@users.noreply.github.com>
2023-03-18 13:34:34 -0400

    About me img and text

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -7,9 +7,9 @@ function HeroSection() {
 
   return (
   <> 
-    <div className='hero-container'> 
-    {/* <img src="src/Components/HeroSection/abm-hero.png" alt="Paris" /> */}
-    <button className="book">Book With Melvin</button>   
+    <div className='hero-container'>
+   <div className='background-image'></div>
+   <button className="book">Book With Melvin</button>   
      <div className='headline'>
      <h1>Melvin Lozada</h1>
      <div className='subtitle'>Get flawless Looks for any Occasion</div>
