72fa2b60efe3259da27255de831f53548c47aa63
aniecres <111096788+aniecres@users.noreply.github.com>
2023-03-19 00:01:52 -0400

    hero section fixed headline and button

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -8,9 +8,10 @@ function HeroSection() {
   return (
   <> 
     <div className='hero-container'>
-   <div className='background-image'></div>
+   <div className='background-image'></div> 
+   <div className='headline'>
    <button className="book">Book With Melvin</button>   
-     <div className='headline'>
+    
      <h1>Melvin Lozada</h1>
      <div className='subtitle'>Get flawless Looks for any Occasion</div>
      </div>
