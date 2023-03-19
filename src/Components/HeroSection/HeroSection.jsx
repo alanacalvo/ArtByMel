3b0fd01a56b97bcb736193afe882705a596a02e3
aniecres <111096788+aniecres@users.noreply.github.com>
2023-03-19 00:55:36 -0400

    finishing touches on about me and hero section elements

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -8,14 +8,15 @@ function HeroSection() {
   return (
   <> 
     <div className='hero-container'>
+   
    <div className='background-image'></div> 
    <div className='headline'>
    <button className="book">Book With Melvin</button>   
-    
-     <h1>Melvin Lozada</h1>
-     <div className='subtitle'>Get flawless Looks for any Occasion</div>
-     </div>
+   <h1>Melvin Lozada</h1>
+    <div className='subtitle'>Get flawless Looks for any Occasion</div>
     </div>
+    </div> 
+    {/* <div></div> */}
   </>
   )
 }
