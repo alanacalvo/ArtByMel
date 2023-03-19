ffb58e52279336d7ff8a2f17355f27dcb42b1cb0
aniecres <111096788+aniecres@users.noreply.github.com>
2023-03-19 17:33:46 -0400

    worked on about/hero sec borders

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Style from './HeroSection.scss';
+import './HeroSection.scss';
 
 
 function HeroSection() {
@@ -8,15 +8,14 @@ function HeroSection() {
   return (
   <> 
     <div className='hero-container'>
-   
    <div className='background-image'></div> 
    <div className='headline'>
    <button className="book">Book With Melvin</button>   
    <h1>Melvin Lozada</h1>
     <div className='subtitle'>Get flawless Looks for any Occasion</div>
     </div>
+     {/* <div className='divider'/> */}
     </div> 
-    {/* <div></div> */}
   </>
   )
 }
