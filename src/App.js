61388a4bd2167b0df26c37cbe2a27e3aea995ede
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2023-03-20 18:21:40 -0400

    Merged changes from main branch and refactored code in hero & about sections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.scss";
-import Header from "./Components/Header";
+import "./Components/HeroSection/HeroSection.jsx";
+import HeroSection from "./Components/HeroSection/HeroSection.jsx";
+import "./Components/AboutSection/AboutSection.jsx";
+import AboutSection from "./Components/AboutSection/AboutSection.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <HeroSection />
+      <AboutSection />
     </div>
   );
 }
