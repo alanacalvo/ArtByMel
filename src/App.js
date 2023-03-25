c58ebfc61e9a497a89d4b1900db07186ee952ccf
alanacalvo <alana.keeton@gmail.com>
2023-03-25 10:17:02 -0400

    beginning work on services buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.scss';
+import HomePage from './Views/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
-        <h1>Art By Mel</h1>
+        <HomePage/>
     </div>
   );
 }
---
+++ src/Components/ReusableComponents/ServicesCards.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function ServicesCards() {
+
+
+
+  return (
+    <div>
+          {/* <Link to={'bridal'} className='link'> */}
+          <div className='card1'>
+            <img src="Rectangle 14.png" alt="bridal makeup" />
+            <p>Bridal Makeup</p>
+          </div>
+        {/* </Link> */}
+        {/* <Link to={'glam'} className='link'> */}
+          <div className='card2'>
+            <img src="Rectangle 15.png" alt="glam makeup" />
+          </div>
+        {/* </Link> */}
+        {/* <Link to={'sfx'} className='link'> */}
+          <div className='card3'>
+            <img src="Rectangle 17.png" alt="SFX makeup" />
+          </div>
+        {/* </Link> */}
+    </div>
+  )
+}
+
+export default ServicesCards
