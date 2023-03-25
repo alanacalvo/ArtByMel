14ee157cd12939f0c86d21350571f8ef94f47e43
alanacalvo <alana.keeton@gmail.com>
2023-03-25 15:26:11 -0400

    changed component heirarchy to make app more clean

--- src/Views/HomePage/HomePage.jsx
+++ src/Views/HomePage/HomePage.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import './HomePage.scss'
+import HeroSection from "../../Components/HeroSection/HeroSection.jsx";
+import AboutSection from "../../Components/AboutSection/AboutSection.jsx";
 import ServicesSection from '../../Components/ServicesSection/ServicesSection'
+import TestimonialsSection from '../../Components/TestimonialsSection/TestimonialsSection';
 
 function HomePage() {
   return (
-    <>  
-    <ServicesSection/>
-    </>
+    <div className='homePage'>
+      <HeroSection />
+      <div className="section-divider hero-divider"></div>
+      <ServicesSection />
+      <TestimonialsSection/>
+      <div className="section-divider about-divider"></div>
+      <AboutSection />
+    </div>
   )
 }
 
