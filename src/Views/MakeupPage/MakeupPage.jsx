d154eecc9f5f426027013fb803bf1fef3ae47014
aniecres <111096788+aniecres@users.noreply.github.com>
2023-03-27 20:09:34 -0400

    fixing verything

---
+++ src/Views/MakeupPage/MakeupPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./MakeupPage.scss"
+
+function MakeupPage() {
+ return (
+    <>
+        <div className="makeup-container">
+            <div className="headliner">
+                <h3>gallery</h3>
+
+            </div>
+        </div>
+    </>
+ );
+}
+
+export default MakeupPage;
