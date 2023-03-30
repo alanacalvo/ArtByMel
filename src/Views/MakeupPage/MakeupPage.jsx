b4aafa09b11b2002e7fe607905f566f378c8ca5c
aniecres <111096788+aniecres@users.noreply.github.com>
2023-03-30 17:07:07 -0400

    added and fixed gallery grid code, imported photos to gallery grid all on makeupPage jsx/scss

--- src/Views/MakeupPage/MakeupPage.jsx
+++ src/Views/MakeupPage/MakeupPage.jsx
@@ -2,37 +2,45 @@ import React from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import "./MakeupPage.scss"
-// import "./Photos"
+import photo1 from "./Photos/photo1.png";
+import photo2 from "./Photos/photo2.png";
+import photo3 from "./Photos/photo3.png";
+import photo4 from "./Photos/photo4.png";
+import photo5 from "./Photos/photo5.png";
+import photo6 from "./Photos/photo6.png";
+import photo7 from "./Photos/photo7.png";
+import photo8 from "./Photos/photo8.png";
+import photo9 from "./Photos/photo9.png";
+import photo10 from "./Photos/photo10.png";
 
 function MakeupPage() {
  return (
     <>
     <Header /> 
         <div className="makeup-container">
-            <div className="section-title"><h3>gallery</h3>
+            <div className="section-title"><h3>gallery</h3></div>
             <div className="headline"><h2>A gallery of stunning looks</h2></div>
-                <div className="subtitle"><subheading>Created by Melvin Lozado</subheading></div>
-
-                <div class="gallery">
-<div class="div1"><img src="./Photos/photo1.jpg"/></div>
-<div class="div2"><img src="./Photos/photo2.jpg"/></div>
-<div class="div3"> </div>
-<div class="div4"> </div>
-<div class="div5"> </div>
-<div class="div6"> </div>
-<div class="div7"> </div>
-<div class="div8"> </div>
-<div class="div9"> </div>
-<div class="div10"> </div>
-<div class="div11"> </div>
-<div class="div12"> </div>
-<div class="div13"> </div>
-<div class="div14"> </div>
-<div class="div15"> </div>
-<div class="div16"> </div>
+                <div className="subtitle">Created by Melvin Lozado</div>
+               
+                <div className="gallery">
+<div className="look-img div1"><img src={photo1} alt="makeup design"/></div>
+<div className="look-img div2"><img src={photo2} alt="makeup design"/></div>
+<div className="look-img div3"><img src={photo3} alt="makeup design"/> </div>
+<div className="look-img div4"><img src={photo4} alt="makeup design"/> </div>
+<div className="look-img div5"><img src={photo5} alt="makeup design"/> </div>
+<div className="look-img div6"><img src={photo6} alt="makeup design"/> </div>
+<div className="look-img div7"><img src={photo7} alt="makeup design"/> </div>
+<div className="look-img div8"><img src={photo8} alt="makeup design"/> </div>
+<div className="look-img div9"><img src={photo9} alt="makeup design"/> </div>
+<div className="look-img div10"><img src={photo10} alt="makeup design"/> </div>
+<div className="look-img div11"><img src={photo1} alt="makeup design"/> </div>
+<div className="look-img div12"><img src={photo1} alt="makeup design"/> </div>
+<div className="look-img div13"><img src={photo1} alt="makeup design"/> </div>
+<div className="look-img div14"><img src={photo1} alt="makeup design"/> </div>
+<div className="look-img div15"><img src={photo1} alt="makeup design"/> </div>
+<div className="look-img div16"><img src={photo1} alt="makeup design"/> </div>
 </div>
 
-</div>
 
          
         </div>
