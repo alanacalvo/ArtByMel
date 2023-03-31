87f881b5f1de61688e85fe2d96d61bd97c6cc10f
aniecres <111096788+aniecres@users.noreply.github.com>
2023-03-30 21:04:45 -0400

    imported sfx page tp App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import HomePage from "./Views/HomePage/HomePage";
 import MakeupPage from "./Views/MakeupPage/MakeupPage";
+import SfxPage from "./Views/SfxPage/SfxPage";
 
 function App() {
   return (
     <div className="App">
      <HomePage />
      <MakeupPage />
+     <SfxPage />
     </div>
   );
 }
