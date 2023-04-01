df53860d581c19dd357218b2e2ad1d860a64d0d2
aniecres <111096788+aniecres@users.noreply.github.com>
2023-03-31 22:27:39 -0400

    editing div tags/ importing photos to theatre jsx

--- src/Views/TheatrePage/TheatrePage.jsx
+++ src/Views/TheatrePage/TheatrePage.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import Footer from "../../Components/Footer/Footer";
 import Header from "../../Components/Header/Header";
 import "./TheatrePage.scss";
+import rec1 from "./TP photos/Rectangle 1.png";
+import rec2 from "./TP photos/Rectangle 2.png";
+import rec3 from "./TP photos/Rectangle 3.png";
+import rec4 from "./TP photos/Rectangle 4.png";
+import rec5 from "./TP photos/Rectangle 5.png";
+import rec6 from "./TP photos/Rectangle 6.png";
+import rec7 from "./TP photos/Rectangle 7.png";
+import rec8 from "./TP photos/Rectangle 8.png";
+import rec9 from "./TP photos/Rectangle 9.png";
+import rec10 from "./TP photos/Rectangle 10.png";
 
 function TheatrePage() {
     return (
@@ -11,19 +21,19 @@ function TheatrePage() {
             <div className="section-title"><h3>gallery</h3></div>
             <div className="headline"><h2>A Showcase of Spectacular Costume Creations</h2></div>
                 <div className="subtitle">Created by Melvin Lozado</div>
-{/* 
+
             <div className="sfx-gallery">
-                <div className="look-img div1"><img src={photo1} alt="makeup design"/></div>
-                <div className="look-img div2"><img src={photo2} alt="makeup design"/></div>
-                <div className="look-img div3"><img src={photo3} alt="makeup design"/> </div>
-                <div className="look-img div4"><img src={photo4} alt="makeup design"/> </div>
-                <div className="look-img div5"><img src={photo11} alt="makeup design"/> </div>
-                <div className="look-img div6"><img src={photo6} alt="makeup design"/> </div>
-                <div className="look-img div7"><img src={photo7} alt="makeup design"/> </div>
-                <div className="look-img div8"><img src={photo8} alt="makeup design"/> </div>
-                <div className="look-img div9"><img src={photo9} alt="makeup design"/> </div>
-                <div className="look-img div10"><img src={photo10} alt="makeup design"/> </div>
-            </div> */}
+                <div className="TP-img div1"><img src={rec1} alt="makeup design"/></div>
+                <div className="TP-img div2"><img src={rec2} alt="makeup design"/></div>
+                <div className="TP-img div3"><img src={rec3} alt="makeup design"/> </div>
+                <div className="TP-img div4"><img src={rec4} alt="makeup design"/> </div>
+                <div className="TP-img div5"><img src={rec5} alt="makeup design"/> </div>
+                <div className="TP-img div6"><img src={rec6} alt="makeup design"/> </div>
+                <div className="TP-img div7"><img src={rec7} alt="makeup design"/> </div>
+                <div className="TP-img div8"><img src={rec8} alt="makeup design"/> </div>
+                <div className="TP-img div9"><img src={rec9} alt="makeup design"/> </div>
+                <div className="TP-img div10"><img src={rec10} alt="makeup design"/> </div>
+            </div>
 
 
         </div>      
