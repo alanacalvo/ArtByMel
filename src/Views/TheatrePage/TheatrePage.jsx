a809e62e7e7f3640e0ac815d6dd3cfa7e6288b62
aniecres <111096788+aniecres@users.noreply.github.com>
2023-03-31 22:01:35 -0400

    updated scss code and imported scss into jsx

--- src/Views/TheatrePage/TheatrePage.jsx
+++ src/Views/TheatrePage/TheatrePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "../../Components/Footer/Footer";
 import Header from "../../Components/Header/Header";
+import "./TheatrePage.scss";
 
 function TheatrePage() {
     return (
@@ -10,6 +11,27 @@ function TheatrePage() {
             <div className="section-title"><h3>gallery</h3></div>
             <div className="headline"><h2>A Showcase of Spectacular Costume Creations</h2></div>
                 <div className="subtitle">Created by Melvin Lozado</div>
+{/* 
+            <div className="sfx-gallery">
+                <div className="look-img div1"><img src={photo1} alt="makeup design"/></div>
+                <div className="look-img div2"><img src={photo2} alt="makeup design"/></div>
+                <div className="look-img div3"><img src={photo3} alt="makeup design"/> </div>
+                <div className="look-img div4"><img src={photo4} alt="makeup design"/> </div>
+                <div className="look-img div5"><img src={photo11} alt="makeup design"/> </div>
+                <div className="look-img div6"><img src={photo6} alt="makeup design"/> </div>
+                <div className="look-img div7"><img src={photo7} alt="makeup design"/> </div>
+                <div className="look-img div8"><img src={photo8} alt="makeup design"/> </div>
+                <div className="look-img div9"><img src={photo9} alt="makeup design"/> </div>
+                <div className="look-img div10"><img src={photo10} alt="makeup design"/> </div>
+                <div className="look-img div11"><img src={photo16} alt="makeup design"/> </div>
+                <div className="look-img div12"><img src={photo12} alt="makeup design"/> </div>
+                <div className="look-img div13"><img src={photo13} alt="makeup design"/> </div>
+                <div className="look-img div14"><img src={photo14} alt="makeup design"/> </div>
+                <div className="look-img div15"><img src={photo15} alt="makeup design"/> </div>
+                <div className="look-img div16"><img src={photo5} alt="makeup design"/> </div>
+            </div> */}
+
+
         </div>      
         <Footer />
         </>
