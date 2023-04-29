4f0bf698f93324f24513a6783fa824e670bad585
alanacalvo <alana.keeton@gmail.com>
2023-04-28 22:37:53 -0400

    commit merge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import "./App.scss";
+import HomePage from "./Views/HomePage/HomePage";
+import MakeupPage from "./Views/MakeupPage/MakeupPage";
+import SfxPage from "./Views/SfxPage/SfxPage";
+import TheatrePage from "./Views/TheatrePage/TheatrePage";
 import "./Components/HeroSection/HeroSection.jsx";
 import "./Components/AboutSection/AboutSection.jsx";
 import './App.scss';
@@ -7,7 +11,10 @@ import HomePage from './Views/HomePage/HomePage';
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <HeroSection />
+      <div className="section-divider hero-divider"></div>
+      <div className="section-divider about-divider"></div>
+      <AboutSection />
     </div>
   );
 }
