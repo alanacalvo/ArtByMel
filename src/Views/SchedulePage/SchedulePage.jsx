5ac7ce4215443181276a090cc2656aa989d9b6fc
alanacalvo <alana.keeton@gmail.com>
2024-02-28 10:12:17 -0500

    calendly widget mobile responsive.

--- src/Views/SchedulePage/SchedulePage.jsx
+++ src/Views/SchedulePage/SchedulePage.jsx
@@ -4,26 +4,52 @@ import { InlineWidget, PopupWidget } from "react-calendly";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import CustomRequestForm from "../../Components/CustomRequestForm/CustomRequestForm";
+import { useMediaQuery } from "@mui/material";
 
 function SchedulePage() {
+  const isSmallScreen = useMediaQuery("(max-width: 768px)");
+
   return (
     <>
       <div className="book-container">
         <Header />
         <div className="calendar">
-          <h1>Book An Appointment</h1>
-          <div className="background">
-            <InlineWidget
-              rootElement={document.getElementById("root")}
-              text="Click here to schedule!"
-              url="https://calendly.com/melvinjr329"
-              styles={{
-                height: "1000px",
-                width: "60vw",
-              }}
-            />
-          </div>
+          {!isSmallScreen ? (
+            <>
+              <h1>Book An Appointment</h1>
+              {/* <div className="background"> */}
+              <InlineWidget
+                rootElement={document.getElementById("root")}
+                text="Click here to schedule!"
+                url="https://calendly.com/melvinjr329"
+                styles={{
+                  height: "1000px",
+                  width: "90vw",
+                }}
+              />
+              {/* </div> */}
+            </>
+          ) : (
+            <>
+              {/* <div className="background"> */}
+              <h1>Book An Appointment</h1>
+
+              <InlineWidget
+                rootElement={document.getElementById("root")}
+                text="Click here to schedule!"
+                url="https://calendly.com/melvinjr329"
+                styles={{
+                  height: "1000px",
+                  width: "100vw",
+                  margin: "0",
+                  padding: "0",
+                }}
+              />
+              {/* </div> */}
+            </>
+          )}
         </div>
+
         <CustomRequestForm />
         <Footer />
       </div>
