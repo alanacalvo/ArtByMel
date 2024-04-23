ddedb94da5fbc10b26167d17d9c4d622db655487
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2024-04-23 13:40:52 -0400

    Fixed the broken responsive layout on the art page
    For viewport widths between 500px and 599px

--- src/Views/ArtPage/ArtPage.jsx
+++ src/Views/ArtPage/ArtPage.jsx
@@ -22,22 +22,18 @@ const ArtPage = () => {
             <img
               src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/102/908/original/painting1.png?1699064683"
               alt="painting of a man holding a rose in one hand and a letter in the other"
-              className="art-pic"
             />
             <img
               src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/102/909/original/painting2.png?1699064691"
               alt="painting of a man sitting and crying into his hands with a tree growing from his back"
-              className="art-pic"
             />
             <img
               src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/102/915/original/painting-3.jpeg?1699066532"
               alt="painting of an African American woman with blue clothing and a blue headwrap on and gold earrings"
-              className="art-pic"
             />
             <img
               src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/102/916/original/painting-4.jpg?1699066556"
               alt="painting of a bunch of coconuts on a coconut tree"
-              className="art-pic"
             />
           </div>
         </div>
