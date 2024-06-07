6e482320b375dc656b1ea82b354608e8b014e486
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2024-06-07 13:45:37 -0400

    Changed the UI of the contact form

--- src/Views/SchedulePage/SchedulePage.jsx
+++ src/Views/SchedulePage/SchedulePage.jsx
@@ -34,7 +34,6 @@ function SchedulePage() {
             </>
           ) : (
             <>
-              <h1>Book An Appointment</h1>
               <InlineWidget
                 rootElement={document.getElementById("root")}
                 text="Click here to schedule!"
